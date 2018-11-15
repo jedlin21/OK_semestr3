d6086fe7f82184b937ba9799cc9051e93f3cb375
I <jedlikowski.pat@gmail.com>
2018-11-15 22:34:56 +0100

    Some plots

---
+++ Sprawozdanie/Python/plots.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 20:57:07 2018
+
+@author: i
+"""
+
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+
+
+def importData(fileName):
+    my_data = np.genfromtxt(fileName, delimiter=';')
+    return my_data
+
+
+def strojeniePlot(strojenieData = './strojenie_test.txt'):    
+    plotableData = importData(strojenieData)
+    
+    plotableStrojenie = dict()
+    for x in range(1, 11):
+        plotableStrojenie[x] = dict()
+        for y in [250,500,750,1000]:
+            plotableStrojenie[x][y] = list()
+        
+    for row in plotableData:
+        plotableStrojenie[int(row[0])][int(row[1])].append(row[3])
+        
+    for x in range(1, 11):
+        for y in [250,500,750,1000]:
+            plotableStrojenie[x][y] =  np.mean(plotableStrojenie[x][y])
+            
+    X = range(1,11)
+    Y = dict()
+    for instances in [250,500,750,1000]:
+        Y[instances] = list()
+    for rangeValue in plotableStrojenie:        
+        for instances in [250,500,750,1000]:
+            Y[instances].append(plotableStrojenie[rangeValue][instances])
+            
+    for key in Y.keys():
+        plt.title("Seeking for GRASP list size", fontsize=18)
+        plt.xlabel("GRASP list size", fontsize=17)
+        plt.ylabel("Distance", fontsize=17)
+        plt.plot(X, Y[key], ".-")
+    plt.legend(("250","500","750","1000"), loc='best', title="Instances")
+    plt.show()
+
+
+if __name__ == '__main__':
+    strojeniePlot()
+    
+    trucskDirec = './Trucks.txt'
+    plotableData = importData(trucskDirec)
+    
+    
+    Instances = list()
+    XTrucks = list()
+    XDistance = list()
+    for row in range(len(plotableData)):
+        Instances.append(int(plotableData[row][0]))
+        XTrucks.append(int(plotableData[row][1]))
+        XDistance.append(plotableData[row][2])
+    
+    plt.title("Distances plot", fontsize=18)
+    plt.xlabel("Number of instances", fontsize=17)
+    plt.ylabel("Distance", fontsize=17)
+    plt.plot(Instances, XDistance , ".-")
+    plt.show()
+    
+    plt.title("Truck number plot", fontsize=18)
+    plt.xlabel("Number of instances", fontsize=17)
+    plt.ylabel("Number of trucks", fontsize=17)
+    plt.yticks(range(0,22, 2))
+    plt.plot(Instances, XTrucks , ".-")
+    plt.show()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
---
+++ Sprawozdanie/functions.h
@@ -0,0 +1,15 @@
+
+
+using namespace std;
+
+void printFleet(vector<vector<int>> );
+vector<vector<int>> write(vector<vector<int>> );
+vector<vector<int>> addVisitedFlag(vector<vector<int>> );
+void printTrucks(vector<vector<int>> );
+bool thereAreShopsToVisit(vector<vector<int>> );
+void addTruck(vector<vector<int>> & , int, int, int );
+int findTheBestFit(vector<vector<int>> & , vector<vector<int>> & );
+void updateTheTrackDatabase(vector<int> & , vector<vector<int>> & , int);
+double calculateSumServiceTime(vector<vector<double>> );
+double calculateDistance(vector<double> , vector<int> );
+
